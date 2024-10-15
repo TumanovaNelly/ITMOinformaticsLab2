66a8b4ac060fa6dba498f0f35e3404802fe7673c
TumanovaNelly <2-man.off@mail.ru>
2024-10-15 18:46:39 +0300

    ++comments and renaming

--- convert_ip.bash
+++ convert_ip.bash
@@ -2,42 +2,53 @@
 
 # Проверка на количество аргументов
 if [[ $# -ne 1 ]]; then
-  echo "Передайте ровно один аргумент"
-  exit 1
+    echo "Передайте ровно один аргумент"
+    exit 1
 fi
 
-# Получение IP-адреса из аргумента
-ip_address=$1
 
-# Разделение IP-адреса на слова (разделитель - ".") и их запись в массив octets
-declare -a octets
-IFS="." read -r -a octets <<< "$ip_address"
+# Получение IP-адреса из 1 аргумента
+IP_ADDRESS=$1
+
+
+# Разделение IP-адреса на слова (разделитель - ".") и их запись в массив OCTETS
+declare -a OCTETS
+IFS="." read -r -a OCTETS <<< "$IP_ADDRESS"
+# IFS - глоб. массив разделителей
+# -a для записи каждого слова в отдельную ячеуку массива
+# -r для того, чтобы \ не воспринимался как символ экранирования
+
 
 # Проверка, что введенный IP-адрес состоит из четырех октетов
-if [[ ${#octets[@]} -ne 4 ]]; then
+if [[ ${#OCTETS[@]} -ne 4 ]]; then
   echo "IP-адрес должен состоять из 4х октетов"
   exit 1
 fi
+# array[@] предстаялет собой все элементы array
 
-declare -a octets_binary
-for i in "${!octets[@]}"; do
+
+declare -a OCTETS_BINARY
+for i in "${!OCTETS[@]}"; do
   # Проверка, что текущий октет - натуральное число
-  if ! [[ ${octets[$i]} =~ ^[0-9]+$ ]]; then
+  if ! [[ ${OCTETS[$i]} =~ ^[0-9]+$ ]]; then
     echo "Октеты должны быть натуральными числами"
     exit 1
   fi
 
+
   # Проверка, что текущий октет - число до 255
-  if [[ ${octets[$i]} -ge 256 ]]; then
+  if [[ ${OCTETS[$i]} -ge 256 ]]; then
     echo "Октеты должны быть числами до 255"
     exit 1
   fi
 
-  octets_binary[$i]=$(printf "%08d" "$(echo "obase=2; ${octets[$i]}" | bc)")
+
+  OCTETS_BINARY[$i]=$(printf "%08d" "$(echo "obase=2; ${OCTETS[$i]}" | bc)")
 done
+# !array[@] == range(0, {#array[@]})
 
 
-echo "${octets_binary[0]}.${octets_binary[1]}.${octets_binary[2]}.${octets_binary[3]}"
+echo "${OCTETS_BINARY[0]}.${OCTETS_BINARY[1]}.${OCTETS_BINARY[2]}.${OCTETS_BINARY[3]}"
 
 
 
